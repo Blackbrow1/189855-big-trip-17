c50bdadaa023ebfa7e40cba07a4b79ba456f5e7d
Aleksandr Chernobrov <alesandro83@mail.ru>
2022-04-29 21:16:41 +0300

    Разбиваем камни. Может что-то пропустил, что еще можно разбить

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,18 +5,15 @@ import {render} from '../render.js';
 
 export default class BoardPresenter {
   boardComponent = new TripEventsListView();
-  taskListComponent = new TripEventsItemView();
 
   init = (boardContainer) => {
     this.boardContainer = boardContainer;
 
     render(this.boardComponent, this.boardContainer);
     render(new PointEditView(), this.boardComponent.getElement());
-    render(new TripEventsListView(), this.boardComponent.getElement());
-    // for (let i = 0; i < 3; i++) {
-    //   render(new TaskView(), this.taskListComponent.getElement());
-    // }
 
-    // render(new LoadMoreButtonView(), this.boardComponent.getElement());
+    for (let i = 0; i < 3; i++) {
+      render(new TripEventsItemView(), this.boardComponent.getElement());
+    }
   };
 }
