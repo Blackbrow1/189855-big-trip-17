a79ba6259a47622d1f55d9dae96473da87f94abd
Aleksandr Chernobrov <alesandro83@mail.ru>
2022-05-03 18:13:31 +0300

    Выполняет Шаблонизируй это

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,4 +1,4 @@
-import {generateOfferConstruction} from '../mock/task.js';
+import {generateOfferConstruction} from '../mock/points.js';
 
 export default class PointsModel {
   points = Array.from({length: 3}, generateOfferConstruction);
